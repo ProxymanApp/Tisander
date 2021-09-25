56a70cc9bcd9d0c62f06b5d2eafb6cd9761ba9da
Nghia Tran <vinhnghiatran@gmail.com>
2021-09-25 10:40:31 +0700

    Add SPM

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Tisander",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v14)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Tisander",
+            targets: ["Tisander"])
+    ],
+    targets: [
+        .target(
+            name: "Tisander",
+            path: "Source"),
+    ]
+)
